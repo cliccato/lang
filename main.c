cc94774c569c1a5679e9bf249dacc8ba97c3d172
cliccato <58629115+cliccato@users.noreply.github.com>
2022-06-05 18:46:01 +0200

    update

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 #include "lib/parser.h"
 #include "lib/visitor.h"
 #include "lib/io.h"
-#define EXT ".hello"
+#define EXT ".mc"
 #define LIMIT 50
 
 void help() {
@@ -21,13 +21,13 @@ int main(int argc, char* argv[]) {
     if (argc >= 2){
         for(i = 1; i < argc; i++){
             int len = strlen(argv[i]);
-            char* extension = &argv[i][len-6];
+            char* extension = &argv[i][len-3];
             if(strcmp(extension, EXT) == 0) {
                 Lexer* lexer = init_lexer(file_contents(argv[i]));
                 Parser* parser = init_parser(lexer);
                 Ast* root = parse(parser, parser->scope);
                 Visitor* visitor = init_visitor();
-                Ast* d = visit(visitor, root);
+                visit(visitor, root);
             }
 
             else {
